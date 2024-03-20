54750e6d09649c0bbb18a4e1f100e54e3893fd91
d0hyeon <ehgus6887@gmail.com>
2024-03-20 17:47:55 +0900

    feat: SerializePath 타입 추가

--- src/types.ts
+++ src/types.ts
@@ -4,7 +4,7 @@ export type PathVariable<
   Prefix extends string,
   Postfix extends string = ''
 > = Path extends `${string}/${Prefix}${infer Key}${Postfix}/${infer Tail}`
-  ? Record<Key, PathValue> & PathVariable<Tail, Prefix, Postfix>
+  ? { [key in Key | keyof PathVariable<Tail, Prefix, Postfix>]: string | number }
   : RestPathVariable<Path, Prefix, Postfix>;
 
 type RestPathVariable<
@@ -15,3 +15,24 @@ type RestPathVariable<
   Path extends `${string}/${Prefix}${infer LastKey}${Postfix extends string ? Postfix : ' '}`
   ? Record<LastKey, PathValue>
   : Record<never, never>;
+
+export type SerializePath<
+  Path extends string,
+  Prefix extends string,
+  Postfix extends string,
+  Variable extends Record<string, PathValue> = {}
+> = Path extends `${infer Head}/${Prefix}${infer Key}${Postfix}/${infer Tail}`
+  ? `${SerializePath<Head, Prefix, Postfix, Variable>}/${InferVariableValue<Variable, Key>}/${SerializePath<Tail, Prefix, Postfix, Variable>}`
+  : RestSerializePath<Path, Prefix, Postfix, Variable>
+
+type RestSerializePath<
+  Path extends string,
+  Prefix extends string,
+  Postfix extends string = '',
+  Variable extends Record<string, PathValue> = {}
+> = Path extends `${infer Head}/${Prefix}${infer Key}${Postfix}`
+  ? `${Head}/${InferVariableValue<Variable, Key>}`
+  : Path
+
+type InferVariableValue<Variable extends Record<string, unknown>, Key extends string>
+  = Key extends keyof Variable ? Variable[Key] : string;
