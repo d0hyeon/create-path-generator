0fa7b52461f2621fe0f6e1dfe45730dc3514073a
d0hyeon <ehgus6887@gmail.com>
2024-03-20 17:52:25 +0900

    feat: exports 추가

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import { createPathGenerator, createVariablePattern } from "./createPathGenerator";
-import { PathVariable as PathVariableByPattern } from "./types";
+import { PathVariable as PathVariableByPattern, PathVariableValue } from "./types";
 
 const pattern = createVariablePattern(':');
-
 export const generatePath = createPathGenerator(pattern);
 export type PathVariable<Path extends string> = PathVariableByPattern<
   Path,
   typeof pattern[0],
   typeof pattern[1]
 >;
-export { createPathGenerator, createVariablePattern };
+export {
+  createPathGenerator,
+  createVariablePattern,
+  PathVariableByPattern,
+  PathVariableValue
+};
 
