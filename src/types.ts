a722753f25f34806975f4b2863ff37b78e5ab489
d0hyeon <ehgus6887@gmail.com>
2024-03-23 17:32:40 +0900

    style: 코드 정리

--- src/types.ts
+++ src/types.ts
@@ -2,7 +2,7 @@ export type ParamPattern<Prefix = string, Postfix = string> = Readonly<[Prefix, 
 export type PathVariableValue = string | number;
 export type PathVariable<Path extends string, Pattern extends ParamPattern> =
   Path extends `${string}/${Pattern[0]}${infer Key}${Pattern[1]}/${infer Tail}`
-  ? { [key in Key | keyof PathVariable<Tail, Pattern>]: string | number }
+  ? { [key in Key | keyof PathVariable<Tail, Pattern>]: PathVariableValue }
   : RestPathVariable<Path, Pattern>;
 
 type RestPathVariable<Path extends string, Pattern extends ParamPattern> =
