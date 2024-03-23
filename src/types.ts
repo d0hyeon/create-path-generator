55a7fb5dbebef90e0dde1237158b7f47b196321d
d0hyeon <ehgus6887@gmail.com>
2024-03-23 16:40:45 +0900

    style: 단락 제거

--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,11 @@
 export type ParamPattern<Prefix = string, Postfix = string> = Readonly<[Prefix, Postfix]>;
 export type PathVariableValue = string | number;
-export type PathVariable<Path extends string, Pattern extends ParamPattern,> =
+export type PathVariable<Path extends string, Pattern extends ParamPattern> =
   Path extends `${string}/${Pattern[0]}${infer Key}${Pattern[1]}/${infer Tail}`
   ? { [key in Key | keyof PathVariable<Tail, Pattern>]: string | number }
   : RestPathVariable<Path, Pattern>;
 
-type RestPathVariable<
-  Path extends string,
-  Pattern extends ParamPattern,
-> =
+type RestPathVariable<Path extends string, Pattern extends ParamPattern> =
   Path extends `${string}/${Pattern[0]}${infer LastKey}${Pattern[1]}`
   ? Record<LastKey, PathVariableValue>
   : Record<never, never>;
